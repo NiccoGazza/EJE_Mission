fa0e6afb68852bf70060adcef039ad2f184aaa7d
fvergara39 <francescavergara39@gmail.com>
2021-05-23 18:06:24 +0200

    Issue: datetime?

--- jup2europe.m
+++ jup2europe.m
@@ -9,8 +9,22 @@
 %	~phasing
 %	~considerare saturno come una sfera di raggio maggiorato di 100km
 %	~dare ulteriore delta-v quando si arriva per far combaciare la velocità circolare di saturno con quella della sonda (=> costringere la sonda ad orbitare attorno a Europa)	
+%planet_elements_and_sv1.m
+%addpath(utilityFunctions);
+dep_planet = 5 ;%jupiter
+arr_planet = 10 ;% satellite, Europe
+%data di arrivo sull'orbita planetostazionaria attorno a Giove : 01/01/2030
+%ipotizzo di rimanere sull'orbita almeno 10gg
+[delta_t_A, delta_T_h] = hohmann_phasing(5, 10, [2030 01 15]);
 
-dep_planet = 5 %jupiter
-arr_planet = 10 % satellite, Europe
-[deltaV_h, deltaT_h] = hohmann_transfer(dep_planet, arr_planet)
+[deltaV_h, deltaT_h] = hohmann_transfer(5, 10);
+
+m_E = G*masses(10);
+vc_s = sqrt(mu_E/100); %velocità circolare della sonda sull'orbita attorno a Europa
+
+deltav_orb = vc_s - v_fin; %v_fin velocità all'apocentro dell'ellisse di trasferimento o velocità circolare DI Europa attorno a Giove??
+%CHIEDERE
+
+
+
 
